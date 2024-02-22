4c97257178e2fed369b3236715a55e9e4455f41c
이동호_T6118 <80622859+as9786@users.noreply.github.com>
2024-02-22 21:51:21 +0900

    Bug: debugging merge conflict

--- infer.py
+++ infer.py
@@ -59,14 +59,15 @@ def main(configs):
         model = models.FCN()
     elif model_name.lower() == 'SegNet'.lower():
         model = models.SegNet()
-    elif model_name.lower() == 'UNet'.lower():
-        model = models.UNet()
-    elif model_name.lower() == 'UnetPlusPlus'.lower():
-        model = models.UnetPlusPlus()
     elif model_name.lower() == 'dilatednet':
         model = models.dilated_net()
     elif model_name.lower() == 'Deeplabv3plus'.lower():
         model = models.DeepLabV3Plus()
+    elif model_name.lower() == 'UNet'.lower():
+        model = models.UNet()
+    elif model_name.lower() == 'UnetPlusPlus'.lower():
+        model = models.UnetPlusPlus()
+    
 
     save_path = configs['path']['save_dir']
     model = torch.load(save_path)
