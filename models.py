74c2324ed0634deafdf12a7a7c5c9e2d34112968
이동호_T6118 <80622859+as9786@users.noreply.github.com>
2024-02-22 21:29:30 +0900

    Bug: debugging mere conflict error
    update DeepLabv3+ and DilatedNet
    
    #11

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from torchvision import models
 import segmentation_models_pytorch as smp
 
+
 LEN_CLASS = 29
 
 def FCN():
@@ -16,6 +17,25 @@ def DeepLabV3Plus():
         classes = LEN_CLASS
     )
     return model
+
+
+def UNet():
+    model = smp.Unet(
+        encoder_name = 'resnet101',
+        encoder_weights = 'imagenet',
+        classes = LEN_CLASS
+    )
+    return model
+
+def UnetPlusPlus():
+    model = smp.UnetPlusPlus(
+        encoder_name="resnet50",
+        encoder_weights="imagenet",
+        in_channels=3,
+        classes=LEN_CLASS,
+    )
+    return model
+
 class SegNet(nn.Module):
     def __init__(self, num_classes=LEN_CLASS):
         super(SegNet, self).__init__()
@@ -139,61 +159,6 @@ class SegNet(nn.Module):
         h = self.dcbr1_1(h)
         h = self.score_fr(h)
         return h
-
-def conv_relu(in_ch, out_ch, size=3, rate=1):
-    conv_relu = nn.Sequential(nn.Conv2d(in_channels=in_ch,
-                                        out_channels=out_ch,
-                                        kernel_size=size,
-                                        stride=1,
-                                        padding=rate,
-                                        dilation=rate),
-                              nn.ReLU())
-    return conv_relu
-
-
-class VGG16(nn.Module):
-    def __init__(self):
-        super(VGG16, self).__init__()
-        self.features1 = nn.Sequential(conv_relu(3, 64, 3, 1),
-                                       conv_relu(64, 64, 3, 1),
-                                       nn.MaxPool2d(2, stride=2, padding=0))
-        self.features2 = nn.Sequential(conv_relu(64, 128, 3, 1),
-                                       conv_relu(128, 128, 3, 1),
-                                       nn.MaxPool2d(2, stride=2, padding=0))
-        self.features3 = nn.Sequential(conv_relu(128, 256, 3, 1),
-                                       conv_relu(256, 256, 3, 1),
-                                       conv_relu(256, 256, 3, 1),
-                                       nn.MaxPool2d(2, stride=2, padding=0))
-        self.features4 = nn.Sequential(conv_relu(256, 512, 3, 1),
-                                       conv_relu(512, 512, 3, 1),
-                                       conv_relu(512, 512, 3, 1))
-        self.features5 = nn.Sequential(conv_relu(512, 512, 3, rate=2),
-                                       conv_relu(512, 512, 3, rate=2),
-                                       conv_relu(512, 512, 3, rate=2))
-
-    def forward(self, x):
-        out = self.features1(x)
-        out = self.features2(out)
-        out = self.features3(out)
-        out = self.features4(out)
-        out = self.features5(out)
-        return out
-
-class Classifier(nn.Module):
-    def __init__(self, num_classes):
-        super(Classifier, self).__init__()
-        self.classifier = nn.Sequential(nn.Conv2d(512, 4096, kernel_size=7, dilation=4, padding=12),
-                                        nn.ReLU(),
-                                        nn.Dropout2d(0.5),
-                                        nn.Conv2d(4096, 4096, kernel_size=1),
-                                        nn.ReLU(),
-                                        nn.Dropout2d(0.5),
-                                        nn.Conv2d(4096, num_classes, kernel_size=1)
-                                        )
-
-    def forward(self, x):
-        out = self.classifier(x)
-        return out
 
 class BasicContextModule(nn.Module):
     def __init__(self, num_classes):
